47c802c047b52512a97b3cd1b94d27186c703267
Digtial Stark <digitalstark.workadventure@gmail.com>
2026-08-05 11:07:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -30,15 +30,6 @@ WA.onInit().then(() => {
         .then(() => console.log("✅ ส่งข้อมูล meeting_room สำเร็จ"))
         .catch((err) => console.error("❌ ส่งข้อมูลไม่สำเร็จ", err));
     });
-
-    // ✅ Modal เตือนให้เปิดกล้อง (เด้งกลางจอ ขนาดใหญ่ ไม่ขึ้นกับ zoom)
-    WA.room.area.onEnter("meeting-room").subscribe(() => {
-        WA.ui.modal.openModal({
-            title: "Camera Reminder",
-            src: "https://digitalstark-system.github.io/work-adventure/camera-reminder.html",
-            position: "center",
-            allowApi: true,
-            allowFullScreen: false
         });
     });
 });
